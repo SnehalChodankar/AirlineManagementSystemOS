e3b0ca4da619b057c380893e086d51e677feab03
Snehal Chodankar <snehalchodankar95@gmail.com>
2021-09-13 21:24:09 +0530

    added login module

--- src/main/java/ams/airlinemanagementsystemos/mainController.java
+++ src/main/java/ams/airlinemanagementsystemos/mainController.java
@@ -1,12 +1,18 @@
 package ams.airlinemanagementsystemos;
 
 import java.io.IOException;
+import java.sql.*;
+import java.util.Objects;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -21,17 +27,66 @@ public class mainController {
     @FXML
     private  ImageView ivMain;
 
+    @FXML
+    private TextField tfUsername;
+
+    @FXML
+    private PasswordField tfPassword;
+
     /**
      * onStartClick method is called when the Start button is clicked. This takes the user
      * to airline details page, that is, airline.fxml.
      * */
     @FXML
     void onStartClick(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("airline.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+        String username = tfUsername.getText();
+        String password = tfPassword.getText();
+        Alert a = new Alert(Alert.AlertType.ERROR);
+
+        String query = "SELECT * FROM login WHERE username = '"+username+"'";
+        Connection conn = getConnection();
+
+        try{
+            Statement st = conn.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            if(!rs.next()){
+                a.setHeaderText("Invalid Username");
+                a.setContentText("User doesn't exists in the database!!!");
+                a.show();
+            }
+            else{
+                if(!Objects.equals(rs.getString("password"), password)){
+                    a.setHeaderText("Invalid Password");
+                    a.setContentText("User doesn't exists in the database!!!");
+                    a.show();
+                }
+                else{
+                    Parent root = FXMLLoader.load(getClass().getResource("airline.fxml"));
+                    stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+                    scene = new Scene(root);
+                    stage.setScene(scene);
+                    stage.show();
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+
+    }
+
+    public Connection getConnection(){
+        Connection conn;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/ams", "root", "");
+            System.out.println("Connected to db!!!");
+            return conn;
+        }
+        catch (Exception ex){
+            System.out.println("Error: "+ ex.getMessage());
+            return null;
+        }
     }
 
     @FXML
